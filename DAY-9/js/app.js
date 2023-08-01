2cfbd3c36a6665ddd76893de1aef97b1aed9bc79
chourabi-dev <chourabitahers62@gmail.com>
2023-08-01 15:49:11 +0100

    latest

---
+++ DAY-9/js/app.js
@@ -0,0 +1,15 @@
+var categories = [
+    { id:1, name:"Electronics" },
+    { id:2, name:"Smart phones" },
+    { id:3, name:"TV" },
+    
+    
+]
+
+var produits = [
+    { id:1, name:"HP pavillion" , category:1, price:2300, photoURL:'https://th.bing.com/th/id/R.a9c34fb8f2688cb6e2d73085f9310aeb?rik=nboa4bDgfjsVgA&riu=http%3a%2f%2fvaluenomad.com%2fwp-content%2fuploads%2f2018%2f01%2fhp-pavilion-15-cc110na.jpg&ehk=bVzaOB7yQdbw4OZ%2f%2fTtnIgWtp3anoY1SdbNpxNXa2XY%3d&risl=&pid=ImgRaw&r=0' },
+    { id:2, name:"Samsung s21" , category:2, price:4300, photoURL:'https://th.bing.com/th/id/R.8e0d7c225cc601e0eed3d26580d1fb70?rik=jHl5zanv5dmayg&pid=ImgRaw&r=0' },
+    { id:3, name:"LG smart TV" , category:3, price:1800, photoURL:'https://rukminim1.flixcart.com/image/1664/1664/television/k/x/z/lg-43lh576t-original-imaejnzc6pdxgqkk.jpeg?q=90' },
+    
+    
+];
