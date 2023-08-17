575b61b374f4ecabdd22d2eb935b0d7b2756b0ae
chourabi-dev <chourabitahers62@gmail.com>
2023-08-17 16:41:16 +0100

    latesdt

--- DAY-14/server/index.js
+++ DAY-14/server/index.js
@@ -3,8 +3,51 @@ const app = express()
 const port = 8080
 const parser = require('body-parser');
 const { getProductList, addProduct, getProductListByPrice, updateProduct, deleteProduct } = require('./modules/produit');
+const { createAccount, auth } = require('./modules/user');
 
 app.use( parser.json() );
+
+
+const jwt = require('jsonwebtoken');
+const url = require('url');
+ 
+// INSERT middleware !!!
+
+app.use(function(req,res,next){
+    
+    
+    const pathname = url.parse(req.url,true).pathname;
+
+
+    if( pathname === '/api/user/auth' ){
+        next();
+    }else if( pathname ==='/api/user/create-account' ){
+        next();
+    }else{
+
+        const token = req.headers.authorization;
+
+ 
+        
+        jwt.verify(token, 'secret-key', function(err, decoded) {
+            console.log() // bar
+    
+            if( decoded ){
+                next();
+            }else{
+                res.send({ success:false, message:"Access denied." })
+            }
+        });
+    }
+
+    
+     
+
+ 
+})
+
+
+
 
  
 // GET POST PUT DELETE produit
@@ -37,6 +80,21 @@ app.put('/api/product',(req,res)=>{
 app.delete('/api/product',(req,res)=>{
     deleteProduct(req,res);
 })
+
+
+
+
+
+app.post('/api/user/create-account',(req,res)=>{
+    createAccount(req,res)
+})
+
+
+app.post('/api/user/auth',(req,res)=>{
+    auth(req,res)
+})
+
+
 
 
 
--- DAY-14/server/modules/user.js
+++ DAY-14/server/modules/user.js
@@ -1,5 +1,69 @@
+const mongodb = require('mongodb');
+const dbURL  = 'mongodb://127.0.0.1:27017';
+const dbName = 'testproductsapp';
 // signin
 
 
 
+var jwt = require('jsonwebtoken');
+
+exports.auth = function(req,res){
+    const body = req.body;
+
+
+    mongodb.MongoClient.connect(dbURL).then((server)=>{
+
+        const db = server.db(dbName);
+
+        const user = {
+            username: body.username,
+            password: body.password
+        }
+
+        db.collection("users").findOne(user).then((doc)=>{
+            if( doc != null ){
+                // create token !!!
+
+                                  // data                            // crypto key 
+                var token = jwt.sign(    { username:doc.username }   , 'secret-key');
+
+                res.send({ success:true, token:token })
+
+
+            }else{
+                res.send({ success:false, message:"wrong username or password." })
+            }
+        })
+
+    })
+
+
+}
+
+
+
+
+
 // signup
+
+exports.createAccount = function(req,res){
+    const body = req.body;
+
+
+    mongodb.MongoClient.connect(dbURL).then((server)=>{
+
+        const db = server.db(dbName);
+
+        const user = {
+            username: body.username,
+            password: body.password
+        }
+
+        db.collection("users").insertOne(user).then((doc)=>{
+            res.send({ success:true, message:"account created successfully." })
+        })
+
+    })
+
+
+}
